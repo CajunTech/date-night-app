2b8b2cfa9d19a4389b076bba1ef0741a72cb920a
Jay Reische <ReischeJay@JohnDeere.com>
2021-10-28 12:34:20 -0500

    styling of drink show page completed

--- src/components/DrinkShow.js
+++ src/components/DrinkShow.js
@@ -1,22 +1,23 @@
 import React from "react";
-import './DrinkShow.css'
+import "./DrinkShow.css";
 
 export default function DrinkShow(props) {
   const drink = props.drink;
   console.log(drink);
   return (
-    <div className="drink-container">
-      <h1>Drink Show</h1>
-      <div className="drink-image">
-        <img src={drink.strDrinkThumb} alt="" />
+    <div className="drink-page">
+      <div className="drink-container">
+        {/* <h1>Drink Show</h1> */}
+        <div className="drink-image">
+                  <img src={drink.strDrinkThumb} alt="" />
+                  
+        </div>
+        <div className="drink-name">
+                  <h2>Name: {drink.strDrink}</h2>
+                  <h3>Category: {drink.strCategory}</h3>
+        </div>
+        
       </div>
-      <div className="drink-name">
-        <h2>Name: {drink.strDrink}</h2>
-          </div>
-          <div className="drink-cat">
-          <h3>Category: {drink.strCategory}</h3>
-          </div>
-      
     </div>
   );
 }
