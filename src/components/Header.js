8d166a434397de5d2fa4c925a7eb3e0a0e046f51
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-10-28 10:23:30 -0500

    basic header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
     return (
-        <div>
-          <h1>Header</h1>
+        <div className="header">
+          <img className='headerImg' src='/images/dna_icon.png' alt='dna logo'/>
+          <h1 className='headerText'>Date Night App</h1>
         </div>
     );
 }
