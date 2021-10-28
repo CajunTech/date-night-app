09997d96061ee79047bb068ba6ebb6361d28029d
Jay Reische <ReischeJay@JohnDeere.com>
2021-10-28 08:50:18 -0500

    added drink api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 export default class App extends Component {
   constructor() {
-    super()
-    this.setState = {
+    super();
+    this.state = {
       drink: {},
-    }
+    };
   }
   // when the page renders, make the api call to get random drink
   componentDidMount() {
-    axios.get("http://www.thecocktaildb.com/api/json/v1/1/random.php")
-      .then(response => {
-      this.setState({drink: response.data.drinks})
-    })
-}
-
+    axios
+      .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+      .then((response) => {
+      this.setState({drink: response.data.drinks[0]});
+      });
+  }
 
   render() {
     return (
       <div>
-        <h1>Drink API</h1>
-        <h2>{this.state.drink.strDrink}</h2>
+        
       </div>
     );
   }
