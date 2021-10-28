ee43bef219cadb503dc4064d968ef37559e9d6b7
Jay Reische <ReischeJay@JohnDeere.com>
2021-10-28 11:15:05 -0500

    drink show page details

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     axios
       // when the page renders, make the api call to get random drink
       .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
@@ -24,7 +24,7 @@ export default class App extends Component {
       });
     
     /*get a random meal json from mealdb and assign to meal state */
-    axios
+  axios
       .get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
       .then((response) => {
         this.setState({ drink: response.data.drinks[0] });
--- src/components/DrinkShow.js
+++ src/components/DrinkShow.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export default function DrinkShow(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  const drink = props.drink;
+  console.log(drink);
+  return (
+    <div className="drink-container">
+      <h1>Drink Show</h1>
+      <div className="drink-image">
+        <img src={drink.strDrinkThumb} />
+      </div>
+      <div className="drink-name">
+        <h2>{drink.strDrink}</h2>
+          </div>
+          <div className="drink-cat">
+          <h3>{drink.strCategory}</h3>
+          </div>
+      
+    </div>
+  );
 }
-
