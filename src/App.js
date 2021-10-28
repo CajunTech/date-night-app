6e8233b83d651a0620a565d51a774c3893323519
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-10-28 08:21:54 -0500

    added index file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			meal: {},
+		};
+	}
+
+	componentDidMount = () => {
+		axios
+			.get('https://www.themealdb.com/api/json/v1/1/random.php')
+			.then((response) => {
+				this.setState({meal: response.data.meals[0]});
+			});
+	};
+
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+				<h2>{this.state.meal.strMeal}</h2>
+			</div>
+		);
+	}
 }
