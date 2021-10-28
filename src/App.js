0a84577450f0a00559a0c6d72659307de64d5219
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-10-28 09:12:12 -0500

    formatted

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./App.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      drink: {},
-      meal: {},
-    };
-  }
-  // when the page renders, make the api call to get random drink
-  componentDidMount() {
-    axios
-      .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-      .then((response) => {
-      this.setState({drink: response.data.drinks[0]});
-      });
-    
-    /*get a random meal json from mealdb and assign to meal state */
+	constructor() {
+		super();
+		this.state = {
+			drink: {},
+			meal: {},
+		};
+	}
+	// when the page renders, make the api call to get random drink
+	componentDidMount() {
+		axios
+			.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+			.then((response) => {
+				this.setState({ drink: response.data.drinks[0] });
+			});
+
+		/*get a random meal json from mealdb and assign to meal state */
 		axios
 			.get('https://www.themealdb.com/api/json/v1/1/random.php')
 			.then((response) => {
-				this.setState({meal: response.data.meals[0]});
+				this.setState({ meal: response.data.meals[0] });
 			});
-	};
+	}
 
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
-
+	render() {
+		return <div></div>;
+	}
 }
