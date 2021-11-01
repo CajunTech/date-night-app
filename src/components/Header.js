04e0584c85b1d023ed8f4d263546c62b1dfef8a6
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-11-01 10:17:41 -0500

    added link to homepage from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
-    return (
-        <div className="header">
-          <img className='headerImg' src='/images/dna_icon.png' alt='dna logo'/>
-          <h1 className='headerText'>Date Night App</h1>
-        </div>
-    );
+	return (
+		
+			<div className="header">
+        <Link className='headerLink' to={'/'}>
+				<img className="headerImg" src="/images/dna_icon.png" alt="dna logo" />
+				<h1 className="headerText">Date Night App</h1>
+        </Link>
+			</div>
+		
+	);
 }
 
 export default Header;
